b9705d9e65b61a62497a93823d236409b2877c4c
Liliya-Pishchuk <metelyck@gmail.com>
2022-05-17 00:30:43 +0300

    mobile-menu corrected

--- src/mobile-menu.js
+++ src/mobile-menu.js
@@ -8,5 +8,6 @@
   refs.closeMenuBtn.addEventListener('click', toggleModal);
   function toggleModal() {
     refs.menu.classList.toggle('is-open');
+    document.body.classList.toggle('modal-open');
   }
 })();
